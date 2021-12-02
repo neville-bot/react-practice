f19041156747e584ce64d08afcd43492c0126122
John Nevin <jnevin17@gmail.com>
2021-12-02 10:51:10 -0600

    added function to add inputted elements into a card component, and two function to sort the card into ascending and descending order

--- dynamic-updating/index.js
+++ dynamic-updating/index.js
@@ -15,10 +15,71 @@ function addFlightToCard() {
   const rating = document.createElement("li");
   rating.innerText = `Rating: ${currentFlight.rating}*`;
   const price = document.createElement("li");
-  price.innerText = `Price: ${currentFlight.price}`;
+  price.innerText = `Price: Rs.${currentFlight.price}`;
   // add them to card
   newFlightCard.append(flightName, travelLocations, rating, price);
   // add card to div
   cardContainer.appendChild(newFlightCard);
-  // delete flight from list
+}
+function sortByPrice() {
+  // add logic to sort flights by price. If data-sort is asc,
+  // sort by ascending price, if desc, sort by price descending. then swap
+  // asc to dsc and vice versa.
+  // grab array of flights from cards
+  let flights = Array.from(document.querySelectorAll(".card"));
+  //  check if data-sort is asc or dsc
+  // if asc, sort by ascending price
+  let ascOrDsc = document.querySelector("#sortPrice").dataset.sort;
+  if (ascOrDsc === "asc") {
+    flights.sort(function(a, b) {
+      return (
+        b.children[3].innerText.replace(/[^0-9]/g, "") -
+        a.children[3].innerText.replace(/[^0-9]/g, "")
+      );
+    });
+    document.querySelector("#sortPrice").dataset.sort = "dsc";
+  } else {
+    // if dsc, sort by descending price
+    flights.sort(function(a, b) {
+      return (
+        a.children[3].innerText.replace(/[^0-9]/g, "") -
+        b.children[3].innerText.replace(/[^0-9]/g, "")
+      );
+    });
+    document.querySelector("#sortPrice").dataset.sort = "asc";
+  }
+  const cardContainer = document.querySelector("#flight-items");
+  cardContainer.childNodes.forEach((card) => cardContainer.removeChild(card));
+  flights.forEach((flight) => cardContainer.appendChild(flight));
+}
+
+function sortByRating() {
+  // grab all the cards
+  let flights = Array.from(document.querySelectorAll(".card"));
+  // check the dataset (asc or dsc)
+  let ascOrDsc = document.querySelector("#sortRating").dataset.sort;
+  // if asc, sort method through the price innerText
+  if (ascOrDsc === "asc") {
+    flights.sort(function(a, b) {
+      return (
+        b.children[2].innerText.replace(/[^0-9]/g, "") -
+        a.children[2].innerText.replace(/[^0-9]/g, "")
+      );
+    });
+    // set the sort rating to dsc
+    document.querySelector("#sortRating").dataset.sort = "dsc";
+  } else {
+    // if dsc, sort method throught the price innertext
+    flights.sort(function(a, b) {
+      return (
+        a.children[2].innerText.replace(/[^0-9]/g, "") -
+        b.children[2].innerText.replace(/[^0-9]/g, "")
+      );
+    });
+    // set the sort rating to asc
+    document.querySelector("#sortRating").dataset.sort = "asc";
+  }
+  const cardContainer = document.querySelector("#flight-items");
+  cardContainer.childNodes.forEach((child) => cardContainer.removeChild(child));
+  flights.forEach((flight) => cardContainer.appendChild(flight));
 }
